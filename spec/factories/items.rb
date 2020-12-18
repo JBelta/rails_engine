864ba6f2ba5c126e4feef592c075649f13b870eb
James Belta <james.belta@gmail.com>
2020-12-18 01:37:43 -0700

    Build Factories adding syntax::method to helper

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,4 +1,7 @@
 FactoryBot.define do
   factory :item do
+    name {Faker::Commerce.product_name}
+    description {Faker::Commerce.material}
+    unit_price {Faker::Number.within(range: 1..10)}
   end
 end
