66823363e08e1825d06dc04d87e127ac89325b84
James Belta <james.belta@gmail.com>
2020-12-18 08:01:56 -0700

    still need to work on nested resources

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :items
+      resources :items do
+        resources :merchants, only: [:show]
+      end
       resources :merchants do
-        resources :items, only: [:index]
+        resources :items, as: :merchant, only: [:show]
       end
     end
   end
