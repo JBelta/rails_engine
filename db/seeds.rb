ef0e40a2200035293411a65f44199746f1e519a4
James Belta <james.belta@gmail.com>
2020-12-18 01:37:53 -0700

    removed pry

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,20 +7,22 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'csv'
 
+
 cmd = "pg_restore --verbose --clean --no-acl --no-owner -h localhost -U $(whoami) -d rails_engine_development db/data/rails-engine-development.pgdump"
+
 puts "Loading PostgreSQL Data dump into local database with command:"
 puts cmd
+
 system(cmd)
 
 items_csv = File.read('db/data/items.csv')
 items = CSV.parse(items_csv, headers: true)
 items.each do |item|
-  binding.pry
   i = Item.new
   i.id = item["id"]
   i.name = item["name"]
   i.description = item["description"]
-  i.unit_price = item["unit_price"]
+  i.unit_price = item["unit_price"].to_f
   i.merchant_id = item["merchant_id"]
   i.save
 end
