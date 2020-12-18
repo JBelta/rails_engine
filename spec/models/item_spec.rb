65647947e730ea221513824630e112278eee85d1
James Belta <james.belta@gmail.com>
2020-12-18 03:46:47 -0700

    validations for presence of params

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -5,4 +5,11 @@ describe Item, type: :model do
     it{ should belong_to :merchant }
     it{ should have_many(:invoices).through(:invoice_items)}
   end
+
+  describe "validations" do
+    it {should validate_presence_of :name}
+    it {should validate_presence_of :description}
+    it {should validate_presence_of :unit_price}
+    it {should validate_presence_of :merchant_id}
+  end
 end
