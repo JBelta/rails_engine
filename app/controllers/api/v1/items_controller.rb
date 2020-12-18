aa37382362507f919676bc91608f87d47a041e80
James Belta <james.belta@gmail.com>
2020-12-18 04:02:18 -0700

    def destroy action

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -12,6 +12,10 @@ class Api::V1::ItemsController < ApplicationController
     render json: Item.create(item_params)
   end
 
+  def destroy
+    render json: Item.delete(params[:id])
+  end
+
   private
   def item_params
     params.require(:items).permit(:name, :description, :unit_price, :merchant_id)
