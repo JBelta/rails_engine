ca15ccb65c4f569453892ab436fcee910c0e06ab
James Belta <james.belta@gmail.com>
2020-12-18 01:01:35 -0700

    invoice relationships spec

---
+++ spec/models/invoice_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+describe Invoice, type: :model do
+  describe 'relationships' do
+    it {should belong_to :customer}
+    it {should belong_to :merchant}
+    it {should have_many(:items).through(:invoice_items)}
+
+  end
+end
