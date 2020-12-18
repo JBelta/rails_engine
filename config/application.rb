e422be06bbe86079986645039b1647299f165fbe
James Belta <james.belta@gmail.com>
2020-12-18 08:01:27 -0700

    rack cors

--- config/application.rb
+++ config/application.rb
@@ -32,4 +32,16 @@ module RailsEngine
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
   end
+
+  module CorsExample
+    class Application < Rails::Application
+      # Initialize configuration defaults for originally generated Rails version.
+      config.load_defaults 5.2
+
+      # Settings in config/environments/* take precedence over those specified here.
+      # Application configuration can go into files in config/initializers
+      # -- all .rb files in that directory are automatically loaded after loading
+      # the framework and any gems in your application.
+    end
+  end
 end
